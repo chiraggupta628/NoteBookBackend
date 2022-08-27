30e715e331723a0981e4d220bede0a0195a45478
Chirag Gupta <chiraggupta629@gmail.com>
2022-08-27 21:09:36 +0530

    first commit

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 
 connectToMongo();
 const app = express()
-const port = Process.env.PORT || 3000 ;
+const port =  3000 ;
 app.use(cors());
 
 app.use(express.json())
 
 app.use('/api/v1/auth',require('./routes/auth'));
 app.use('/api/v1/notes',require('./routes/notes'));
+app.get("/",(req,res)=>{
+  res.send("Running");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
